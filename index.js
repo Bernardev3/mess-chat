82be3c8e6d92a2c7c2dc51cf5e1f36c9f38232db
Bernardev3 <118381885+Bernardev3@users.noreply.github.com>
2023-06-04 13:57:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const io = new Server(server);
 const port = process.env.PORT || 3000;
 
 let messages = [];
+let users = [];
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/client/index.html");
@@ -25,20 +26,29 @@ app.get("*", function(req, res) {
 io.on("connection", function(socket) {
 	console.log(`Um usuário com o ID: ${socket.id} se conectou`);
 
+	users.push(socket.id);
+
+	if (messages != []) {
+		socket.emit("receive messages", messages);
+	}
+
 	socket.on("send message", function(data) {
 		if (messages.length >= 25) {
 			messages = [];
 		}
 
 		messages.push(data);
-	});
 
-	socket.on("request messages", function() {
+		users.forEach(function(userID) {
+			socket.to(userID).emit("receive messages", messages);
+		});
+
 		socket.emit("receive messages", messages);
 	});
 
 	socket.on("disconnect", function() {
 		console.log(`Um usuário com o ID: ${socket.id} se desconectou`);
+		users.splice(users.indexOf(socket.id), 1);
 	});
 });
 
