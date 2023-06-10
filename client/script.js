8e84d43b18b43b1652f8e6c20827fd9313ab1b20
Bernardev3 <118381885+Bernardev3@users.noreply.github.com>
2023-06-09 21:33:58 -0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -8,12 +8,6 @@ sendButton.onclick = function() {
 	socket.emit("send message", {author: username.value, message: message.value});
 }
 
-document.addEventListener("keyup", function(event) {
-	if (event.key == "Enter") {
-		socket.emit("send message", {author: username.value, message: message.value});
-	}
-});
-
 socket.on("receive messages", function(messages) {
 	sendedMessages.innerHTML = "";
 
